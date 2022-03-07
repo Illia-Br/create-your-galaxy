24fbc6cef828a9d6ee5eb61f5328e13daf190cff
Illia Brytun <elbrytun@gmail.com>
2022-03-07 12:13:12 -0500

    add middleware to check if user is logged in

--- models/profile.js
+++ models/profile.js
@@ -3,6 +3,8 @@ import mongoose from 'mongoose'
 const profileSchema = new mongoose.Schema({
   name: String,
   avatar: String,
+  planets: [{type: Schema.Types.ObjectId, ref: "Planet"}],
+  galaxies: [{type: Schema.Types.ObjectId, ref: "Galaxy"}]
 }, {
   timestamps: true
 })
