058b32c6eb0d5e74760b2650d4bd4509adda43ca
Illia Brytun <elbrytun@gmail.com>
2022-03-07 14:50:00 -0500

    add show view for profiles

---
+++ routes/profile.js
@@ -0,0 +1,13 @@
+import { Router } from "express"
+import * as profilesCtrl from '../controllers/profiles.js'
+
+const router = Router()
+
+
+
+router.get('/:id', profilesCtrl.show)
+
+
+export {
+  router
+}
